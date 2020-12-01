489ff3a56ae7fa7ec569f7684adb6828b06165d5
gaganVirk <deep.virk@outlook.co.nz>
2020-12-01 21:40:20 +1300

    solved third chapter exercises

---
+++ Python_Projects/Python_Projects.py
@@ -0,0 +1,29 @@
+#3-8 Seeing the world
+locations = ['delhi', 'auckland', 'wellington', 'invercargill']
+
+# print list in original order
+print(locations)
+
+# use sorted() to print your list
+print(sorted(locations))
+
+# print the list in original order
+print(locations)
+
+# use reverse() to change the order
+locations.reverse()
+print(locations)
+
+#use reverse again
+locations.reverse()
+print(locations)
+
+# use sort to change your list alphabetically
+locations.sort()
+print(locations)
+
+# use sort to change your list
+locations.sort(reverse=True)
+print(locations)
+
+print(len(locations))
