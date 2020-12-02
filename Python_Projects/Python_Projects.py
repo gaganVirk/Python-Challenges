952a6c87c6089b805f2e4600b74af68d7f64a8c3
gaganVirk <deep.virk@outlook.co.nz>
2020-12-02 21:45:35 +1300

    added 4 chapter exercise solutions

--- Python_Projects/Python_Projects.py
+++ Python_Projects/Python_Projects.py
@@ -1,29 +1,49 @@
-#3-8 Seeing the world
-locations = ['delhi', 'auckland', 'wellington', 'invercargill']
+# 4_1 Pizzas 
+pizzas = ['vege', 'meat', 'fish']
 
-# print list in original order
-print(locations)
+for pizza in pizzas:
+    print("i like " + pizza)
 
-# use sorted() to print your list
-print(sorted(locations))
+print("i am learning python")
+print("i am loving it!")
 
-# print the list in original order
-print(locations)
+# 4_2 Animals
+animals = ['tiger', 'cat', 'dog']
+for animal in animals:
+    print(animal[0] + " is a dangerous animal.")
+    
+print("Any of those animals would be a great pet.")
 
-# use reverse() to change the order
-locations.reverse()
-print(locations)
+#4_3 Counting to Twenty
+for i in range(1,21):
+    print(i)
 
-#use reverse again
-locations.reverse()
-print(locations)
+#4_4 One Million
 
-# use sort to change your list alphabetically
-locations.sort()
-print(locations)
+for num in range(1,1000000):
+    print(num) 
 
-# use sort to change your list
-locations.sort(reverse=True)
-print(locations)
+# 4_5 Summing a Million
+value = list(range(1,1000001))
+print(min(value))
+print(max(value))
+print(sum(value))
 
-print(len(locations))
+# 4_6 Odd Numbers
+for i  in range(1,20):
+    if i % 2 != 0:
+        print(i)
+
+# 4_7 Threes
+for i in range(1,11):
+    three = i * 3
+    print(three)
+
+# 4_8 Cubes
+for i in range(1, 11):
+    cube = i ** 3
+    print(cube)
+
+# 4_9 Cube Comprehension
+cubes = [value ** 3 for value in range(1,11)]
+print(cubes)
